cd20341b766de1d47977a3e9a071dc1731de0fe4
marienboonman <41787462+marienboonman@users.noreply.github.com>
2023-07-22 17:41:20 +0200

    Update voltage_logger.py

--- voltage_logger.py
+++ voltage_logger.py
@@ -40,6 +40,6 @@ while True:
        
         if n == 360:
            endtime = timestamp
-           filename = ('data from {} to {}.csv'.format(starttime, endtime))
+           filename = ('data/data from {} to {}.csv'.format(starttime, endtime))
            s.to_csv(filename.replace(':','-'))
            break
