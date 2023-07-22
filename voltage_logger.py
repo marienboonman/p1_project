1e85275845937c4b868646c0ea985ea39a011754
marienboonman <41787462+marienboonman@users.noreply.github.com>
2023-07-22 17:40:51 +0200

    Update voltage_logger.py

--- voltage_logger.py
+++ voltage_logger.py
@@ -6,11 +6,13 @@ Created on Sat Jul 22 00:09:14 2023
 """
 from datetime import datetime 
 import pandas as pd
+import time
 
-def get_voltage(url = 'http://192.168.2.6/api/v1/p1port/telegram', timestamp = True):
+def get_voltage(url = 'http://{}/api/v1/p1port/telegram', ip = '192.168.2.6' ,timestamp = True):
     import json
     import requests
 
+    url = url.format(ip)    
     result = requests.get(url)
     telegram = json.loads(result.text)
     parts = telegram[3].split('\n')
@@ -19,7 +21,25 @@ def get_voltage(url = 'http://192.168.2.6/api/v1/p1port/telegram', timestamp = T
             return [float(part.split('(')[1].split('*')[0]), datetime.strptime(telegram[0], "%Y-%m-%d %H:%M:%S")]
             break
         
-s = pd.Series(dtype = float)
+
 while True:
-    voltage, time = get_voltage()
-    s[time] = voltage
+    s = pd.Series(dtype = float, name = 'Spanning (V)')
+    voltage, timestamp = get_voltage()
+    n = 0
+    while True:
+        voltage, timestamp = get_voltage()
+        if timestamp.second%10 == 0:
+           s[timestamp] = voltage
+           time.sleep(7)
+           n = n+1
+           #print('added')
+           if n == 1:
+               starttime = timestamp
+        #else:
+        #   print('waiting')
+       
+        if n == 360:
+           endtime = timestamp
+           filename = ('data from {} to {}.csv'.format(starttime, endtime))
+           s.to_csv(filename.replace(':','-'))
+           break
